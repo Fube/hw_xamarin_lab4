213da062d76993da70ece800ec18ebd63ecb9d75
F249 Master <f249master@champlaincollege.qc.ca>
2021-09-10 13:47:41 -0400

    Init commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public TipModel _model;
+        public MainPage()
+        {
+            InitializeComponent();
+            _model = new TipModel();
+
+            Tuple<string, string>[] rows =
+           {
+                Tuple.Create("Bill: ", "Enter bill"),
+                Tuple.Create("Tip: ", "Enter tip"),
+                Tuple.Create("Total Bill: ", ""),
+                Tuple.Create("Total Tip: ", ""),
+            };
+
+            int i = 0;
+            for(i = 0; i < rows.Length; i++)
+            {
+                var label = new Label();
+                label.Text = rows[i].Item1;
+                grid.Children.Add(label, 0, i);
+
+                View view;
+
+                if(rows[i].Item2.Equals(""))
+                {
+                    view = new Label();
+                }
+                else
+                {
+                    view = new Entry();
+                    (view as Entry).Placeholder = rows[i].Item2;
+                }
+
+                grid.Children.Add(view, 1, i);
+            }
+
+            var calcBtn = new Button();
+            calcBtn.BackgroundColor = Color.Magenta;
+            calcBtn.Text = "CALCULATE BILL";
+            calcBtn.Clicked += CalculateBill;
+            grid.Children.Add(calcBtn, 0, i++);
+
+            var clearBtn = new Button();
+            clearBtn.Clicked += Clear;
+            grid.Children.Add(clearBtn, 0, i++);
+
+        }
+
+        private void CalculateBill(object sender, EventArgs e)
+        {
+            //_model.Bill = decimal.Parse(bill.Text);
+            //_model.Tip = decimal.Parse(tip.Text);
+            //_model.Guests = int.Parse(guests.Text);
+
+            //totalBill.Text = _model.Total.ToString("C2");
+            //totalTip.Text = _model.TotalTip.ToString("C2");
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            _model.Clear();
+            //totalBill.Text = totalTip.Text = bill.Text = tip.Text = guests.Text = "";
+        }
+    }
+}
---
+++ App1/App1/TipModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class TipModel
+    {
+        public decimal Bill;
+        public decimal Tip;
+        public int Guests;
+        public decimal Total 
+        {
+            get
+            {
+                return Bill + Tip;
+            }
+        }
+
+        public decimal TotalTip
+        {
+            get
+            {
+                return Total * (1 + Guests / 100);
+            }
+        }
+
+        public void Clear()
+        {
+            Bill = Tip = default;
+            Guests = default;
+        }
+    }
+}
